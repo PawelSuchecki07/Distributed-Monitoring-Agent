a0ded19b18dd7eb43e35333a97e2446bb29b0363
Paweł Suchecki <115819984+PawelSuchecki07@users.noreply.github.com>
2026-06-13 11:58:42 +0200

    CPU usage fixed

--- src/collectors/system_metrics_collector.cpp
+++ src/collectors/system_metrics_collector.cpp
@@ -14,28 +14,75 @@ std::string SMCollector::getName() {
     return "SMCollector";
 }
 
-std::string SMCollector::getCPU() {
+std::pair<long,long> getCPUinfoFromLine(std::string line){
     int user, nice, system, idle, iowait, irq, softirq;
-    int cpu_used = 0;
-    int cpu_total = 0;
+    long cpu_used, cpu_total;
+
+
+    std::stringstream ss(line);
+    std::string cpu_label;
+
+
+    ss >> cpu_label >> user >> nice >> system >> idle >> iowait >> irq >> softirq;
+    cpu_used = user + nice + system;
+    cpu_total = cpu_used + idle + iowait + irq + softirq;
+    return {cpu_used, cpu_total};
+    
+}
+
+
+std::string SMCollector::getCPU() {
+    std::pair<long,long> result1{0,0}, result2{0,0};
+    std::string line;
+    bool found = false;
+
     std::ifstream file("/proc/stat");
     if (!file.is_open()) {
         return "Error: Unable to open /proc/stat";
     }
-    std::string line;
     while (std::getline(file, line)) {
         if (line.find("cpu ") == 0) {
-            std::stringstream ss(line);
-            std::string cpu_label;
-            ss >> cpu_label >> user >> nice >> system >> idle >> iowait >> irq >> softirq;
-            cpu_used = user + nice + system;
-            cpu_total = cpu_used + idle + iowait + irq + softirq;
-            return std::to_string(float(cpu_used * 100) / cpu_total) + "%";
+            result1 = getCPUinfoFromLine(line);
+            found = true;
+            break;
         }
     }
-    return "Error: cpu metrics not found";
+    file.close();
 
+    if (!found) {
+        return "Error: 'cpu' line not found in /proc/stat";
+    }
+
+    usleep(100000);
+
+    found = false;
+    file.open("/proc/stat");
+    if (!file.is_open()) {
+        return "Error: Unable to open /proc/stat";
+    }
+    while (std::getline(file, line)) {
+        if (line.find("cpu ") == 0) {
+            result2 = getCPUinfoFromLine(line);
+            found = true;
+            break;
+        }
+    }
+    file.close();
+
+    if (!found) {
+        return "Error: 'cpu' line not found in /proc/stat";
+    }
+
+    long delta_used = result2.first - result1.first;
+    long delta_total = result2.second - result1.second;
+
+    if (delta_total == 0) {
+        return "Error: delta_total is zero";
+    }
+
+    return std::to_string((delta_used * 100) / delta_total) + "%";
 }
+
 
 std::string SMCollector::getRAM() {
     std::ifstream file("/proc/meminfo");
